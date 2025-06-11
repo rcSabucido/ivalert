6b6c4b31dad8d88e3569e3d07832dae6982d3d89
Ryz Clyd Sabucido <121755498+rcSabucido@users.noreply.github.com>
2025-06-11 18:49:36 +0800

    #6 Create, animate, and stylize alert component to fit project theme (#8)
    * #6 Create Alert component
    
    * #6 Create animation for alert component
    
    * #6 Remove index header
    
    * #6 Fix alert component stagnation when swiping down
    
    * #6 Fix alert component to be swiped upward
    
    * #6 Add expo font library to project
    
    * #6 Initialize useHook fonts from google-poppins directory
    
    * #6 Stylize alert component to fit theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+import Alert from "../components/Alert";
 
 export default function Index() {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <View
       style={{
@@ -10,6 +14,19 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable onPress={() => setShowAlert(true)}>
+        <Text
+          style={{
+            color: "blue",
+            marginTop: 20,
+            fontFamily: "Poppins_500Medium",
+          }}
+        >
+          Press me!
+        </Text>
+      </Pressable>
+
+      <Alert isVisible={showAlert} onClose={() => setShowAlert(false)}/>
     </View>
   );
 }
