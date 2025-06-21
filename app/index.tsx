0008a2deb1c6ec1c65f79aa4808fee12754c2685
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-06-21 22:51:26 +0800

    #5 Implement home screen UI with styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import Alert from "../components/Alert";
 
 export default function Index() {
   const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <View
+    <View 
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        justifyContent: "flex-start",
+      }}>
+    <ScrollView>
+      <View style={styles.headerBlock}>
+        <Text style={styles.headerText}>WELCOME TO</Text>
+        <Text style={styles.headerSubText}>IVALERT</Text>
+      </View>
+      <View style={styles.block}>
+        <Text style={styles.text}>Tracking Status</Text>
+        <View style={styles.trackingBlock}>
+          <Text style={styles.trackingText}>75{"%"}</Text>
+        </View>
+        <Text style={styles.text}>Bag Volume: 1 Liter</Text>
+      </View>
+      <View style={styles.block}>
+        <Text style={styles.text}>Fluid Level Monitor</Text>
+        <View style={styles.fluidBlock}>
+          <Text style={styles.trackingText}>75{"%"}</Text>
+        </View>
+      </View>
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Pressable onPress={() => setShowAlert(true)}>
         <Text
@@ -27,6 +44,68 @@ export default function Index() {
       </Pressable>
 
       <Alert isVisible={showAlert} onClose={() => setShowAlert(false)}/>
+    </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 32,
+    color: "#263268",
+  },
+  headerSubText: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 64,
+    color: "#009DFF",
+    lineHeight: 53,
+  },
+  headerBlock: {
+    padding: 24,
+    paddingTop: 64,
+    flex: 1,
+  },
+  block: {
+    backgroundColor: "#BBDEFB",
+    padding: 10,
+    marginLeft: 24,
+    marginRight: 24,
+    marginTop: 16,
+    marginBottom: 16,
+    borderRadius: 24,
+    alignItems: "center",
+  },
+  trackingBlock: {
+    margin: 16,
+    borderColor: "black",
+    backgroundColor: "white",
+    borderWidth: 3,
+    borderRadius: 12,
+    height: 150,
+    width: 140,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fluidBlock: {
+    margin: 16,
+    borderColor: "black",
+    backgroundColor: "white",
+    borderWidth: 3,
+    borderRadius: 12,
+    height: 150,
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  trackingText: {
+    color: "#263268",
+    fontFamily: "Poppins_700Bold",
+    fontSize: 40,
+  },
+  text: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 20,
+    color: "#263268",
+  }
+})
