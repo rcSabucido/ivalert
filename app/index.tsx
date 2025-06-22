23d9c315475e673910d61db01b4eafc6a6a5100b
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-06-22 11:31:41 +0800

    #5 Implement home screen UI with styling and gestures (PR: #13)
    * #5 Implement home screen UI with styling
    
    * #5 Create fluid level styles and icon
    
    * #5 Create minor adjustment for FluidLevel component
    
    * #5 Move tracking status block to its own component
    
    * #5 Implement gesture bar functionality for tracking status

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import FluidLevel from "@/components/FluidLevel";
+import TrackingStatus from "@/components/TrackingStatus";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Alert from "../components/Alert";
 
 export default function Index() {
   const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <View
+    <View 
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+      }}>
+      <View style={styles.headerBlock}>
+        <Text style={styles.headerText}>WELCOME TO</Text>
+        <Text style={styles.headerSubText}>IVALERT</Text>
+      </View>
+      <View style={[styles.block]}>
+        <TrackingStatus />
+      </View>
+      <View style={styles.block}>
+        <Text style={styles.text}>Fluid Level Monitor</Text>
+        <FluidLevel level={75} />
+      </View>
+
+      {/*
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Pressable onPress={() => setShowAlert(true)}>
         <Text
@@ -25,8 +37,47 @@ export default function Index() {
           Press me!
         </Text>
       </Pressable>
+      <Text>Testing</Text>
+      <Text>Testing</Text>
+       */}
 
       <Alert isVisible={showAlert} onClose={() => setShowAlert(false)}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 32,
+    color: "#263268",
+  },
+  headerSubText: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 64,
+    color: "#009DFF",
+    lineHeight: 53,
+  },
+  headerBlock: {
+    padding: 24,
+    paddingTop: 64,
+    flex: 1,
+  },
+  block: {
+    backgroundColor: "#BBDEFB",
+    padding: 12,
+    marginLeft: 24,
+    marginRight: 24,
+    marginTop: 16,
+    marginBottom: 16,
+    borderRadius: 24,
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  text: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 20,
+    lineHeight: 24,
+    color: "#263268",
+  }
+})
